7505ea52f3351d9d67f08fd7a99f0cd335b9d3f2
Cristian Butzke <cristian.btzk@gmail.com>
2024-05-05 20:18:32 -0300

    Feat: users table

---
+++ db/migrations/20240505T210000_create_users.ts
@@ -0,0 +1,17 @@
+import { Kysely, sql } from 'kysely';
+
+export async function up(db: Kysely<unknown>): Promise<void> {
+  await db.schema
+    .createTable('users')
+    .addColumn('id', 'uuid', (col) => col.primaryKey())
+    .addColumn('name', 'varchar', (col) => col.notNull())
+    .addColumn('email', 'varchar', (col) => col.notNull())
+    .addColumn('password', 'varchar', (col) => col.notNull())
+    .addColumn('created_at', 'timestamp', (col) => col.defaultTo(sql`now()`).notNull())
+    .addColumn('updated_at', 'timestamp', (col) => col.defaultTo(sql`now()`).notNull())
+    .execute();
+}
+
+export async function down(db: Kysely<unknown>): Promise<void> {
+  await db.schema.dropTable('users').execute();
+}
--- db/migrations/20240505T230000_create_topics.ts
+++ db/migrations/20240505T230000_create_topics.ts
@@ -6,8 +6,11 @@ export async function up(db: Kysely<unknown>): Promise<void> {
     .addColumn('id', 'bigserial', (col) => col.primaryKey())
     .addColumn('title', 'varchar', (col) => col.notNull())
     .addColumn('completed', 'boolean', (col) => col.defaultTo(false))
+    .addColumn('user_id', 'uuid', (col) => col.references('users.id').onDelete('set null').onUpdate('cascade').notNull())
     .addColumn('created_at', 'timestamp', (col) => col.defaultTo(sql`now()`).notNull())
     .execute();
+
+  await db.schema.createIndex('topic_author_id').on('topics').column('user_id').execute();
 }
 
 export async function down(db: Kysely<unknown>): Promise<void> {
